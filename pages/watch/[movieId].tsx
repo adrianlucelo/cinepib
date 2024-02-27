38db73fb0670ea00209ef459c2ece41241f46c3d
Adrian Lucelo <adrianlucelo193@gmail.com>
2024-02-27 19:04:35 -0300

    Corrigir a função isYouTubeURL para especificar o tipo de parâmetro

--- pages/watch/[movieId].tsx
+++ pages/watch/[movieId].tsx
@@ -10,7 +10,7 @@ const Watch = () => {
   const { data } = useMovie(movieId as string);
   
   // Função para verificar se a URL é do YouTube e extrair o ID do vídeo
-  const isYouTubeURL = (url) => {
+  const isYouTubeURL = (url: string) => {
     const youtubeRegex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
     const match = url.match(youtubeRegex);
     return match ? match[1] : null;
